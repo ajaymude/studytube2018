62f2dc46d5ea1cce2d44057315ae597ee1b9aab2
ajaymude <mudeajay@gmail.com>
2025-06-08 21:17:33 +0530

    auth get current user

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./component/Header";
 import Footer from "./component/Footer";
 import { Outlet } from "react-router-dom";
 import { useGetUserQuery } from "./store/auth/authApiSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { setCredentials } from "./store/auth/authSlice";
 
 const App = () => {
-    const { data: user, isLoading, isError } = useGetUserQuery();
+    const dispatch = useDispatch();
+  const { userInfo } = useSelector(s => s.auth);
+  const { data: user, isLoading, isError } = useGetUserQuery();
+
+  console.log(user?.data)
+
+  useEffect(() => {
+    if (user) dispatch(setCredentials(user?.data));
+  }, [user, dispatch]);
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* Header */}
@@ -27,4 +38,3 @@ const App = () => {
 };
 
 export default App;
-
